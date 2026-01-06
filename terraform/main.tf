0f5d8fdbfeb6b025e509886e6a717161d7d5d82b
marciocardozodev <60279810+marciocardozodev@users.noreply.github.com>
2026-01-06 23:09:50 +0000

    Initial InfraK8s Terraform skeleton and CI/CD workflow

---
+++ terraform/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+# TODO: adicionar aqui os recursos de Kubernetes (EKS, namespaces, ingress etc.)
