3223a4eaf3322f9286752a2e1608da0fc170fa23
marciocardozodev <60279810+marciocardozodev@users.noreply.github.com>
2026-02-04 22:07:40 +0000

    feat: atualizar nome do bucket do Terraform para oficina-cardozo-terraform-state-sp

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "oficina-cardozo-terraform-state"
+    bucket  = "oficina-cardozo-terraform-state-sp"
     key     = "eks/prod/terraform.tfstate"
     region  = "sa-east-1"
     encrypt = true
